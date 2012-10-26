d14a6795ff02967e9d2a0075e2d151d6afba799a
Antoine-V <antoine.veron@eleves.ec-nantes.fr>
2012-10-26 14:33:50 +0200

    Test

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Antoine_V
+ *
+ * Created on 26 octobre 2012, 14:31
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
